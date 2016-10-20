136a3bb6baea24078952cccef063a853e9caafd6
Eric Howard <howard@nlc.org>
2016-10-20 11:53:47 -0400

    Inital version of the install script

---
+++ install-packages.R
@@ -0,0 +1,24 @@
+# install-packages.R
+
+# Script to install my most commonly used R Packages
+
+install.package("ggplot2")
+install.package("ggmap")
+install.package("nnet")
+install.package("lme4")
+install.package("lmetest")
+install.package("knitr")
+install.package("devtools")
+install.package("stargazer")
+install.package("RPostgreSQL")
+install.package("RMySQL")
+install.package("roxygen")
+install.package("rgeos")
+install.package("rgdal")
+install.package("maps")
+install.package("maptools")
+install.package("xtable")
+install.package("Hmisc")
+install.package("shiny")
+install.package("dplyr")
+install.package("tidyr")
